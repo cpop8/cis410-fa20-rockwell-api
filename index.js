27d1ddbb47301436f4854bb5ae82b382153c10a9
cpop8 <cpop8@colostate.edu>
2020-11-03 18:37:10 -0700

    10.1 10/27 /movies/:pk

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const db = require('./dbConnectExec.js')
 
 const app = express();
 
@@ -6,4 +7,41 @@ app.get("/hi",(req,res)=>{
     res.send("hello world")
 })
 
+app.get("/movies", (req,res)=>{
+    //get data from database
+    db.executeQuery(`SELECT * 
+    FROM movie 
+    LEFT JOIN genre 
+    ON genre.genrePK = movie.GenreFK`)
+    .then((result)=>{
+        res.status(200).send(result)
+    })
+    .catch((err)=>{
+        console.log(err);
+        res.status(500).send()
+    })
+})
+app.get("/movies/:pk", (req, res)=>{
+    var pk = req.params.pk
+    //console.log("my PK:", pk)
+
+    var myQuery = `SELECT * 
+    FROM movie 
+    LEFT JOIN genre 
+    ON genre.genrePK = movie.GenreFK
+    WHERE moviePK = ${pk}`
+
+    db.executeQuery(myQuery)
+    .then((movies)=>{
+       // console.log("Movies: ", movies)
+
+       if(movies[0]){
+           res.send(movies[0])
+       }else{res.status(404).send('bad request')}
+    })
+    .catch((err)=>{
+        console.log("Error in /movies/pk", err)
+        res.status(500).send()
+    })
+})
 app.listen(5000,()=>{console.log("app is running on port 5000")})
